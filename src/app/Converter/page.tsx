88d5a37b5a8cdd7480862e2848ed2d881370fb52
chiscookeke11 <chiscookeke11@gmail.com>
2025-09-10 14:15:08 +0100

    added the button and functionality for switching currencies

--- src/app/Converter/page.tsx
+++ src/app/Converter/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ArrowRightLeft } from "lucide-react";
 import { FormEvent, useEffect, useState } from "react"
 
 
@@ -23,6 +24,8 @@ export default function Page() {
     const [rates, setRates] = useState<RatesData | null>(null)
     const number = 10
     const [selectedToken, setSelectedToken] = useState<keyof RatesData>("aptos")
+    const [fromCurrency, setFromCurrency] = useState<"USD" | "APTOS">("APTOS")
+    const [toCurrency, setToCurrency] = useState<"USD" | "APTOS">("USD")
 
 
 
@@ -34,9 +37,9 @@ export default function Page() {
 
 
         else {
-           const price =  rates?.[selectedToken]?.usd * number
-           console.log(price)
-           return price
+            const price = rates?.[selectedToken]?.usd * number
+            console.log(price)
+            return price
         }
     }
 
@@ -58,24 +61,51 @@ export default function Page() {
     }, [])
 
 
+
+    const switchCurrencies = () => {
+        if (fromCurrency === "APTOS" && toCurrency === "USD") {
+            setFromCurrency("USD")
+            setToCurrency("APTOS")
+        }
+        else if (fromCurrency === "USD" && toCurrency === "APTOS") {
+            setFromCurrency("APTOS")
+            setToCurrency("USD")
+        }
+        else return;
+    }
+
+
     console.log(rates?.aptos?.usd)
 
+
+    console.log("from currency:", fromCurrency)
+    console.log("to currency:", toCurrency)
 
 
     return (
         <div className="w-full h-screen flex items-center justify-center px-4 py-5 font-space-grotesk " >
-            <form onSubmit={calcPrice} className="w-full max-w-3xl bg-white rounded-xl h-full py-7 px-5 flex flex-col items-center gap-4 text-black" >
+            <form onSubmit={calcPrice} className="w-full max-w-2xl bg-white rounded-xl h-full py-7 px-5 flex flex-col items-center gap-4 text-black" >
                 <h4 className="font-bold text-xl md:text-3xl ">Aptos Converter</h4>
 
-                <input type="number" placeholder="3" />
+
 
 
 
                 <div className="w-full flex items-center justify-between" >
-                    <p>Aptos</p>
+                    {/* from-input */}
+                    <label htmlFor="" className="flex flex-col items-center gap-1 p-2  " >
+                        <span>{fromCurrency}</span>
+                        <input type="text" className="border border-black outline-none w-full  p-3 text-lg font-semibold rounded-md " />
+                    </label>
+
+                    <button type="button" className="mt-4" onClick={switchCurrencies} >    <ArrowRightLeft size={40} /></button>
 
 
-                    <p>USd</p>
+                    {/* to input  */}
+                    <label htmlFor="" className="flex flex-col items-center gap-1 p-2  ">
+                        <span>{toCurrency}</span>
+                        <input type="text" className="border border-black outline-none w-full  p-3 text-lg font-semibold rounded-md " />
+                    </label>
 
                 </div>
 
